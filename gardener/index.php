d506e81e865b3c0750c538e2a35ab07310bc5b29
Meeran Husain <meeranhusain3@gmail.com>
2026-04-15 23:15:28 +0530

    Initial commit

--- gardener/index.php
+++ gardener/index.php
@@ -11,6 +11,7 @@
     <meta content="" name="description">
 
     <?php include_once("includes/css.php"); ?>
+
 </head>
 
 <body>
@@ -50,7 +51,7 @@
 
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
             <h1 class="m-0">Gardener</h1>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
