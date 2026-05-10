17dd40924e5e6a3e0e59c548eee11b421ee34780
Meeran Husain <meeranhusain3@gmail.com>
2026-05-10 20:45:29 +0530

    admin panel

---
+++ gardener/admin/admin_includes/sidebar.php
@@ -0,0 +1,89 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">
+                <span class="menu-title">Dashboard</span>
+                <i class="mdi mdi-home menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Slider</span>
+                <i class="menu-arrow"></i> 
+              </a>
+              <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Add Slider</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All Sliders</a></li>
+                </ul>
+              </div>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#services" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Services</span>
+                <i class="menu-arrow"></i> 
+              </a>
+              <div class="collapse" id="services">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Add Services</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All Services</a></li>
+                </ul>
+              </div>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#project_categories" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Project Category</span>
+                <i class="menu-arrow"></i> 
+              </a>
+              <div class="collapse" id="project_categories">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Add Project Categories</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All Project Categories</a></li>
+                </ul>
+              </div>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#projects" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Projects</span>
+                <i class="menu-arrow"></i> 
+              </a>
+              <div class="collapse" id="projects">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Add Projects</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All Projects</a></li>
+                </ul>
+              </div>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#teams" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Teams</span>
+                <i class="menu-arrow"></i> 
+              </a>
+              <div class="collapse" id="teams">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Add Teams</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All Teams</a></li>
+                </ul>
+              </div>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#testimonials" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Testimonials</span>
+                <i class="menu-arrow"></i> 
+              </a>
+              <div class="collapse" id="testimonials">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Add Testimonials</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">All Testimonials</a></li>
+                </ul>
+              </div>
+            </li>
+
+          </ul>
+        </nav>
